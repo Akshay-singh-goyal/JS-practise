f54ca71d3d8fad2517f46c07bc48d38256db84a3
Akshay-singh-goyal <152581106+Akshay-singh-goyal@users.noreply.github.com>
2025-07-08 19:55:49 +0000

    array part 2

---
+++ JS/Array.js
@@ -0,0 +1,75 @@
+//we can use all the nummber and string we are write in the array
+//In array we are use most element 
+// how we get the arrayobject 
+
+// const myArr = [0,1,2,3,4]
+// // console.log(myArr[1]);
+// const myArr1 = ["akshay","praveen","virendra","deepu"]
+// // console.log(myArr1);
+
+// //If we want to add in the array so we can use .push to add
+// //let see by example
+// myArr1.push("somiya")
+// console.log(myArr1)
+
+// //how we can remove in the array
+// //let see by example
+// //If we only remove in the array only the last value
+// myArr1.pop("praveen")
+// console.log(myArr1);
+
+
+// //if we can frist value in the array so we can use the unshift
+// //let see by the examle
+
+// myArr1.unshift("somiya")
+// console.log(myArr1);
+
+
+// //If we want to remove frist value in the array we can use shift operation to reomve
+// //let see be the example
+
+// myArr1.shift("somiya")
+// console.log(myArr1);
+
+
+// //If we want to seacrh in the value we can use the includes 
+// //let see by example
+
+// console.log(myArr1.includes("akshay"));
+
+
+// //If we want to chang into string 
+
+// const newarr = myArr1.join()
+// console.log(myArr1);
+// console.log(newarr);
+
+//Now we can study about the slice and the splice both arediffrent in the slice it can start form value inert in the arrray and original aray are same
+//but in the splice it remove the object we can use in the spilce it can remove and change in the array 
+//Let we can udnerstand by the example
+
+// let myarr2 = [1,2,3,4,5,6]
+// console.log("A" + myarr2);
+//  let myarr3 = myarr2.slice(1,3)
+//  console.log("B"+myarr3);
+// //  let myarr4 = myarr2.splice(1,2)
+// //  console.log("C"+myarr4);
+//  console.log(myarr2);
+ 
+
+// When we can merge two array in one frm it can bemerge but the new array condider 2nd whole array in one data
+//let see what we can say through the thory
+
+// let arr= ["akshay","somiya","vivek","papa"]
+// let arr2 = ["mummuy","didi","chotidi"]
+// let newarr = arr.push(arr2)
+// console.log(arr);
+// console.log(newarr);
+
+//let we can use to concat to single differnt value for each and every value
+//we can not understand by the theroy so we can use the udnerstand by the examle
+
+// let myarr = arr.concat(arr2)
+// console.log(myarr);
+console.log("akshay");
